e536db126f6e9eef8b2107306b96a38ecb81690c
gleb1524 <gleb1524@yandex.ru>
2022-08-11 09:48:19 +0300

    Домашняя работа к первому уроку

---
+++ lesson1-servlet-api/src/main/java/ru/karaban/ProductServlet.java
@@ -0,0 +1,74 @@
+package ru.karaban;
+
+import ru.karaban.persist.Product;
+import ru.karaban.persist.ProductRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/product/*")
+public class ProductServlet extends HttpServlet {
+
+    private ProductRepository productRepository;
+
+    @Override
+    public void init() throws ServletException {
+        this.productRepository = new ProductRepository();
+        for (int i = 1; i <= 10; i++) {
+            this.productRepository.insert(new Product("Product " + i, (int) (Math.random() * (i + 100))));
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<table>");
+        writer.println("<tr>");
+        writer.println("<th>id</th>");
+        writer.println("<th>title</th>");
+        writer.println("<th>cost</th>");
+        writer.println("</tr>");
+
+
+        for (Product product : productRepository.findAll()) {
+
+            if (null == request.getPathInfo()) {
+                writer.println("<tr>");
+                writer.println("<td><a href='" + getServletContext().getContextPath() + "/product/" + product.getId() + "'>" + product.getId() + "</a></td>");
+                writer.println("<td>" + product.getTitle() + "</td>");
+                writer.println("<td>" + product.getCost() + "</td>");
+                writer.println("</tr>");
+            } else {
+                String[] token = request.getPathInfo().split("/");
+                long id;
+                try {
+                    id = Long.parseLong(token[1]);
+                } catch (Exception e) {
+                    id = -1;
+                }
+                if ("/".equals(request.getPathInfo()) || productRepository.findById(id) == null) {
+                    String path = "/404";
+                    ServletContext servletContext = getServletContext();
+                    RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+                    requestDispatcher.forward(request, response);
+                }
+                if (request.getPathInfo().equals("/" + product.getId())) {
+                    writer.println("<tr>");
+                    writer.println("<td><a href='" + getServletContext().getContextPath() + "/product/" + product.getId() + "'>" + product.getId() + "</a></td>");
+                    writer.println("<td>" + product.getTitle() + "</td>");
+                    writer.println("<td>" + product.getCost() + "</td>");
+                    writer.println("</tr>");
+                }
+            }
+        }
+
+        writer.println("</table>");
+    }
+}
---
+++ lesson1-servlet-api/src/main/java/ru/karaban/persist/Product.java
@@ -0,0 +1,38 @@
+package ru.karaban.persist;
+
+import java.util.HashMap;
+
+public class Product {
+    private long id;
+    private String title;
+    private int cost;
+
+    public Product(String title, int cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
