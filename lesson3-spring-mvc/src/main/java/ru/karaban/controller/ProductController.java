3b0809046f3cb33309d37acd276f51f02f360ea9
gleb1524 <gleb1524@yandex.ru>
2022-08-18 17:11:59 +0300

    Вопрос по 3 дз

---
+++ lesson3-spring-mvc/src/main/java/ru/karaban/controller/ProductController.java
@@ -0,0 +1,35 @@
+package ru.karaban.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.karaban.persist.Product;
+import ru.karaban.persist.ProductRepository;
+
+@Controller
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    public String listPage(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "product";
+    }
+
+    @GetMapping("/{id}")
+    public String form(@PathVariable("id") long id, Model model) {
+        model.addAttribute("product", productRepository.findById(id));
+        return "product_form";
+    }
+
+    @PostMapping
+    public String saveProduct(Product product) {
+        productRepository.update(product);
+        return "redirect:/product";
+    }
+
+}
